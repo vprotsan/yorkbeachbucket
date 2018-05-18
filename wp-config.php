425673781da1d35fb16ef67cdd2fd552075e1129
Jeremy Michael Cerda <jcerda@repequity.com>
2018-05-18 14:38:56 -0400

    write_acces_debug

--- wp-config.php
+++ wp-config.php
@@ -159,3 +159,7 @@ if ( !defined('ABSPATH') )
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
+
+define( 'FS_METHOD', 'direct' );
+define('FS_CHMOD_DIR', 0770);
+define('FS_CHMOD_FILE', 0660);
